af98473e46499489633041537a1f55336de3e523
hyemin610 <hyemin610@naver.com>
2023-08-22 15:57:32 +0900

    마이페이지 탭버튼 CSS

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -4,6 +4,7 @@ import Main from "../pages/Main";
 import SignUp from "../pages/SignUp";
 import Layout from "./Layout";
 import Signin from "../pages/Signin";
+import Mypage from "../pages/Mypage";
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Main />} />
+          <Route path="/Mypage" element={<Mypage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<Signin />} />
         </Route>
