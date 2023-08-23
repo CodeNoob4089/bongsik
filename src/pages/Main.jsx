3a7f04cecf9fa5e02a95dfe87e20342049e9cf82
hyemin610 <hyemin610@naver.com>
2023-08-23 15:04:01 +0900

    style/footer 하단고정, 메인페이지 위치 조정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,17 +3,22 @@ import Map from "../components/Map";
 import useAuthStore from "../store/auth";
 import { signOut } from "firebase/auth";
 import MyList from "../components/MyList";
+import { styled } from "styled-components";
 
 function Main() {
   const user = useAuthStore((state) => state.user);
   const navigate = useNavigate();
 
   return (
-    <div>
+    <Container>
       <Map />
       <MyList />
-    </div>
+    </Container>
   );
 }
 
 export default Main;
+
+const Container = styled.div`
+  display: flex;
+`;
