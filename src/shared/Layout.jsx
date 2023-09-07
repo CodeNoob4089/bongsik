6a6cc44f1afc74c610361fffbf10df73980c440e
김채현 <kimchennie@naver.com>
2023-09-07 17:50:03 +0900

    style/헤더 수정, 메인페이지 레이아웃

--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -34,17 +34,15 @@ console.log(MypageCss)
   return (
     <LayoutContainer style={{ MinHeigh: MypageCss ? "100vh" :"auto" }}>
       <Header>
-        <LogoContent>
           <LogoImg
             src={
-              "https://firebasestorage.googleapis.com/v0/b/kimbongsik-69c45.appspot.com/o/%EB%A1%9C%EA%B3%A07.png?alt=media&token=b0943697-3adc-40ab-9bec-fe12259408e1"
+              "https://firebasestorage.googleapis.com/v0/b/kimbongsik-69c45.appspot.com/o/ETG%20%E1%84%83%E1%85%A2%E1%84%86%E1%85%AE%E1%86%AB%E1%84%8C%E1%85%A1%E1%84%89%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%85%E1%85%A9%E1%84%80%E1%85%A9.png?alt=media&token=ee0210fe-744c-40ef-a806-be5ba8fc08fc"
             }
             onClick={() => {
               setCurrentPage("")
               navigate("/main");
             }}
           />
-        </LogoContent>
         {isLoggedIn ? (
           <>
             <NavigationBar>
@@ -56,7 +54,7 @@ console.log(MypageCss)
                navigate("/");
              }}
              currentPage={currentPage}
-            >사이트 소개</Button>
+            >About</Button>
               <Button
                 id="community"
                 onClick={() => {
@@ -88,7 +86,7 @@ console.log(MypageCss)
                navigate("/");
              }}
              currentPage={currentPage}
-            >사이트 소개</Button>
+            >About</Button>
             <Button
               id="signin"
               onClick={() => {
@@ -136,10 +134,9 @@ const LayoutContainer = styled.div`
 const Button = styled.button`
   border: none;
   height: 60px;
-  width: 120px;
-  font-size: 18px;
-  color: ${(props) => props.id === props.currentPage? "red": "black"};
-  background-color: rgba(0,0,0,0);
+  width: 5rem;
+  color: ${(props) => props.id === props.currentPage? "#FF4E50": "black"};
+  background: none;
   cursor: pointer;
 `;
 const NavigationBar = styled.div`
@@ -147,23 +144,24 @@ const NavigationBar = styled.div`
 `;
 
 const Header = styled.div`
+  font-size: 0.8rem;
   background: white;
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  height: 6rem;
-  padding: 0 20px;
+  height: 3.5rem;
+  padding: 0 6rem;
 `;
 
-const LogoContent = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-`;
+// const LogoContent = styled.div`
+//   display: flex;
+//   align-items: center;
+//   gap: 20px;
+// `;
+
 const LogoImg = styled.img`
-  height: 37px;
-  margin-left: 0.5rem;
+  height: 1.7rem;
   cursor: pointer;
 `;
 
